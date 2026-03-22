50753b5b13ccf459f088a26b9408060d6ea54b30
shimizu <shimizu1018@gmail.com>
2026-03-22 21:25:34 +0900

    feat: プロジェクトを新規作成

---
+++ src/main/java/jp/co/shimizu/phoneorderapi/PhoneOrderApiApplication.java
@@ -0,0 +1,13 @@
+package jp.co.shimizu.phoneorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneOrderApiApplication.class, args);
+    }
+
+}
